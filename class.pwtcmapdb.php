ec6824a8ccc83b9b3b70b5bc978f791c6e4c5249
Mark Hartel <mark_hartel@hotmail.com>
2018-03-05 19:57:20 -0800

    refactor code

--- class.pwtcmapdb.php
+++ class.pwtcmapdb.php
@@ -3,6 +3,7 @@
 class PwtcMapdb {
 
 	const MAP_POST_TYPE = 'ride_maps';
+	const START_LOCATION_FIELD = 'start_location';
 	const TERRAIN_FIELD = 'terrain';
 	const LENGTH_FIELD = 'length';
 	const MAX_LENGTH_FIELD = 'max_length';
@@ -10,15 +11,22 @@ class PwtcMapdb {
 	const MAP_TYPE_FIELD = 'type';
 	const MAP_LINK_FIELD = 'link';
 	const MAP_FILE_FIELD = 'file';
+	const COPY_ANCHOR_LABEL = '<i class="fa fa-files-o"></i>';
+	const FILE_ANCHOR_LABEL = '<i class="fa fa-download"></i>';
+	const LINK_ANCHOR_LABEL = '<i class="fa fa-link"></i>';
 
 /*
 	const MAP_POST_TYPE = 'route';
+	const START_LOCATION_FIELD = 'start_location';
 	const TERRAIN_FIELD = 'route_terrain';
 	const LENGTH_FIELD = 'route_length';
 	const MAX_LENGTH_FIELD = 'max_route_length';
 	const MAP_TYPE_FIELD = 'map_type';
 	const MAP_LINK_FIELD = 'map_link';
 	const MAP_FILE_FIELD = 'map_file';
+	const COPY_ANCHOR_LABEL = 'Copy';
+	const FILE_ANCHOR_LABEL = 'File';
+	const LINK_ANCHOR_LABEL = 'Link';
 */
     private static $initiated = false;
 
@@ -61,8 +69,6 @@ class PwtcMapdb {
 	public static function get_distance($post_id) {
 		$length = get_field(self::LENGTH_FIELD, $post_id);
 		$max_length = get_field(self::MAX_LENGTH_FIELD, $post_id);
-		//self::write_log($length);
-		//self::write_log($max_length);
 		if ($max_length == '') {
 			return $length . ' miles';
 		}
@@ -73,7 +79,6 @@ class PwtcMapdb {
 
 	public static function get_terrain($post_id) {
 		$terrain = get_field(self::TERRAIN_FIELD, $post_id);
-		//self::write_log($terrain);
 		$result = '';
 		foreach ($terrain as $item) {
 			$result .= strtoupper($item);
@@ -85,68 +90,81 @@ class PwtcMapdb {
 		$url = '';
 /*
 		$type = get_field(self::MAP_TYPE_FIELD, $post_id);
-		//self::write_log($type);
 		if ($type == 'file') {
 			$file = get_field(self::MAP_FILE_FIELD, $post_id);
-			//self::write_log($file);
-			$url = '<a target="_blank" href="' . $file['url'] . '">File</a>';
+			$url = '<a title="Download map file." target="_blank" href="' . $file['url'] . '">' . self::FILE_ANCHOR_LABEL . '</a>';
 		}
 		else if ($type == 'link') {
 			$link = get_field(self::MAP_LINK_FIELD, $post_id);
-			//self::write_log($link);
-			$url = '<a target="_blank" href="' . $link . '">Link</a>';
+			$url = '<a title="Go to map link." target="_blank" href="' . $link . '">' . self::LINK_ANCHOR_LABEL . '</a>';
 		}
 */
 
 		while (have_rows(self::MAP_FIELD, $post_id) ): the_row();
 			$type = get_sub_field(self::MAP_TYPE_FIELD);
-			//self::write_log($type);
 			if ($type == 'file') {
 				$file = get_sub_field(self::MAP_FILE_FIELD);
-				//self::write_log($file);
-				$url = '<a target="_blank" href="' . $file['url'] . '">File</a>';
+				$url = '<a target="_blank" href="' . $file['url'] . '">' . self::FILE_ANCHOR_LABEL . '</a>';
 			}
 			else if ($type == 'link') {
 				$link = get_sub_field(self::MAP_LINK_FIELD);
-				//self::write_log($link);
-				$url = '<a target="_blank" href="' . $link . '">Link</a>';
+				$url = '<a target="_blank" href="' . $link . '">' . self::LINK_ANCHOR_LABEL . '</a>';
 			}
 		endwhile;
 	
 		return $url;
 	}
 
-	public static function count_maps($title, $startswith) {
+	public static function count_maps($title, $startswith, $bylocation) {
 		global $wpdb;
 		$search_title = $title . '%';
 		if ($startswith == 'false') {
 			$search_title = '%' . $search_title;
 		}
-		$sql_stmt = $wpdb->prepare(
-			'select count(ID)' . 
-			' from ' . $wpdb->posts .
-			' where post_title like %s and post_type = %s and post_status = \'publish\'', 
-			$search_title, self::MAP_POST_TYPE);
+		if ($bylocation == 'true') {
+			$sql_stmt = $wpdb->prepare(
+				'select count(p.ID)' . 
+				' from ' . $wpdb->posts . ' as p inner join ' . $wpdb->postmeta . 
+				' as m on p.ID = m.post_id where p.post_type = %s and p.post_status = \'publish\'' . 
+				' and m.meta_key = %s and m.meta_value like %s', 
+				self::MAP_POST_TYPE, self::START_LOCATION_FIELD, $search_title);	
+		}
+		else {
+			$sql_stmt = $wpdb->prepare(
+				'select count(ID)' . 
+				' from ' . $wpdb->posts .
+				' where post_title like %s and post_type = %s and post_status = \'publish\'', 
+				$search_title, self::MAP_POST_TYPE);
+		}
 		$results = $wpdb->get_var($sql_stmt);
 		return $results;
 	}
 
-	public static function fetch_maps($title, $startswith, $offset = -1 , $rowcount = -1) {
+	public static function fetch_maps($title, $startswith, $bylocation, $offset = -1 , $rowcount = -1) {
 		global $wpdb;
 		$search_title = $title . '%';
 		if ($startswith == 'false') {
 			$search_title = '%' . $search_title;
 		}
-		$sql_stmt = $wpdb->prepare(
-			'select ID, post_title' . 
-			' from ' . $wpdb->posts .
-			' where post_title like %s and post_type = %s and post_status = \'publish\'' . 
-			' order by post_title', 
-			$search_title, self::MAP_POST_TYPE);
+		if ($bylocation == 'true') {
+			$sql_stmt = $wpdb->prepare(
+				'select p.ID, p.post_title' . 
+				' from ' . $wpdb->posts . ' as p inner join ' . $wpdb->postmeta . 
+				' as m on p.ID = m.post_id where p.post_type = %s and p.post_status = \'publish\'' . 
+				' and m.meta_key = %s and m.meta_value like %s order by p.post_title', 
+				self::MAP_POST_TYPE, self::START_LOCATION_FIELD, $search_title);	
+		}
+		else {
+			$sql_stmt = $wpdb->prepare(
+				'select ID, post_title' . 
+				' from ' . $wpdb->posts .
+				' where post_title like %s and post_type = %s and post_status = \'publish\'' . 
+				' order by post_title', 
+				$search_title, self::MAP_POST_TYPE);
+		}
 		if ($offset >= 0 and $rowcount >= 0) {
 			$sql_stmt .= ' limit ' . $offset . ',' . $rowcount;
 		}
-		//self::write_log($sql_stmt);
 		$results = $wpdb->get_results($sql_stmt, ARRAY_A);
 		return $results;
 	}
@@ -172,24 +190,28 @@ class PwtcMapdb {
 	public static function lookup_maps_callback() {
 		if (false) {
 			$response = array(
-				'error' => 'You are not allowed to search route maps.'
+				'error' => 'You are not allowed to search the map library.'
 			);
 			echo wp_json_encode($response);
 		}
-		else if (!isset($_POST['title']) or !isset($_POST['startswith']) or !isset($_POST['limit'])) {
+		else if (!isset($_POST['title']) or !isset($_POST['startswith']) or !isset($_POST['bylocation']) or !isset($_POST['limit'])) {
 			$response = array(
-				'error' => 'Input parameters needed to search route maps are missing.'
+				'error' => 'Input parameters needed to search map library are missing.'
 			);
 			echo wp_json_encode($response);
 		}
 		else {
 			$title = sanitize_text_field($_POST['title']);
+			$bylocation = $_POST['bylocation'];
 			$limit = intval($_POST['limit']);	
 			$startswith = 'false';
 			if (isset($_POST['startswith'])) {
 				$startswith = trim($_POST['startswith']);
 			}
-			$nmaps = intval(self::count_maps($title, $startswith));
+			if (empty($title) and $bylocation == 'true') {
+				$bylocation = 'false';
+			}
+			$nmaps = intval(self::count_maps($title, $startswith, $bylocation));
 			$message = '';
 			if (isset($_POST['count']) and intval($_POST['count']) != $nmaps) {
 				$message = 'Search results have changed, paging context was lost.';
@@ -204,7 +226,7 @@ class PwtcMapdb {
 						$offset = intval($_POST['offset']) + $limit;
 					}
 				}
-				$maps = self::fetch_maps($title, $startswith, $offset, $limit);
+				$maps = self::fetch_maps($title, $startswith, $bylocation, $offset, $limit);
 				$return_maps = self::build_map_array($maps);
 				$response = array(
 					'count' => $nmaps,
@@ -216,7 +238,7 @@ class PwtcMapdb {
 				echo wp_json_encode($response);
 			}
 			else {
-				$maps = self::fetch_maps($title, $startswith);
+				$maps = self::fetch_maps($title, $startswith, $bylocation);
 				$return_maps = self::build_map_array($maps);
 				$response = array(
 					'maps' => $return_maps);
@@ -238,7 +260,7 @@ class PwtcMapdb {
 		$a = shortcode_atts(array('limit' => 0), $atts);
 		$current_user = wp_get_current_user();
 		if ( 0 == $current_user->ID ) {
-			return 'Please log in to search the route map database.';
+			return 'Please log in to search the map library.';
 		}
 		else {
 			ob_start();
@@ -246,16 +268,32 @@ class PwtcMapdb {
 	<script type="text/javascript">
 		jQuery(document).ready(function($) { 
 			function populate_maps_table(maps) {
+				var copylink = '<a title="Copy ride title to clipboard." class="copy-btn"><?php echo self::COPY_ANCHOR_LABEL ?></a>';
 				$('.pwtc-mapdb-maps-div').append('<table class="pwtc-mapdb-rwd-table">' +
-					'<tr><th>Title</th><th>Distance</th><th>Terrain</th><th>Media</th></tr>' +
+					'<tr><th>Title</th><th>Distance</th><th>Terrain</th><th>Actions</th></tr>' +
 					'</table>');
 				maps.forEach(function(item) {
 					$('.pwtc-mapdb-maps-div table').append(
 						'<tr postid="' + item.ID + '">' + 
-						'<td data-th="Title">' + item.title + '</td>' +
+						'<td data-th="Title"><span>' + item.title + '</span></td>' +
 						'<td data-th="Distance">' + item.distance + '</td>' +
 						'<td data-th="Terrain">' + item.terrain + '</td>' +
-						'<td data-th="Media">' + item.media + '</td></tr>');    
+						'<td data-th="Actions">' + copylink + ' ' + item.media + '</td></tr>');    
+				});
+				$('.pwtc-mapdb-maps-div table .copy-btn').on('click', function(evt) {
+					var title = $(this).parent().parent().find('td').first().find('span').first()[0];
+					if (window.getSelection().rangeCount > 0) window.getSelection().removeAllRanges();
+					var range = document.createRange();  
+  					range.selectNode(title);  
+  					window.getSelection().addRange(range);  
+					try {  
+    					var successful = document.execCommand('copy');  
+    					var msg = successful ? 'successful' : 'unsuccessful';  
+    					console.log('Copy title command was ' + msg);  
+  					} catch(err) {  
+    					console.log('Oops, unable to copy');  
+  					}  
+					window.getSelection().removeAllRanges();  
 				});
 			}
 
@@ -313,7 +351,7 @@ class PwtcMapdb {
 						populate_maps_table(res.maps);
 					}
 					else {
-						$('.pwtc-mapdb-maps-div').append('<div>No route maps found.</div>');					
+						$('.pwtc-mapdb-maps-div').append('<div>No maps found.</div>');					
 					}
 				}
 				$('body').removeClass('pwtc-mapdb-waiting');
@@ -322,14 +360,19 @@ class PwtcMapdb {
 			function load_maps_table(mode) {
 				var title = $(".pwtc-mapdb-search-sec .search-frm input[name='title']").val().trim();
 				var startswith = false;
-				if ($(".pwtc-mapdb-search-sec .search-frm input[name='startswith']").is(':checked')) {
+				if ($('.pwtc-mapdb-search-sec .search-frm .searchfrom').val() == 'begin') {
 					startswith = true;
+				}
+				var bylocation = false;
+				if ($('.pwtc-mapdb-search-sec .search-frm .searchby').val() == 'location') {
+					bylocation = true;
 				}
 				var action = $('.pwtc-mapdb-search-sec .search-frm').attr('action');
 				var data = {
 					'action': 'pwtc_mapdb_lookup_maps',
 					'title': title,
 					'startswith': startswith,
+					'bylocation': bylocation,
 					'limit': <?php echo $a['limit'] ?>
 				};
 				if (mode != 'search') {
@@ -361,15 +404,21 @@ class PwtcMapdb {
 		});
 	</script>
 	<div class='pwtc-mapdb-search-sec'>
-	<p>To search the route map database, press the <strong>Search</strong> button. 
-	To narrow your search, first enter a string into the <strong>Title</strong> field before searching.</p>
+	<div>To browse the map library, press the <strong>Search</strong> button. 
+	To narrow your search, enter a string into the <strong>Search For</strong> field before searching.</div>
 	<form class="search-frm pwtc-mapdb-stacked-form" action="<?php echo admin_url('admin-ajax.php'); ?>" method="post">
-		<span>Title</span>
+		<span>Search By</span>
+		<select class="searchby">
+            <option value="title" selected>Ride Title</option>
+			<!--<option value="location">Start Location</option>-->
+        </select>		
+		<span>Search For</span>
 		<input type="text" name="title"/>
-		<span>Title Begins With</span>
-		<span class="pwtc-mapdb-checkbox-wrap">
-			<input type="checkbox" name="startswith"/>
-		</span>
+		<span>Search From</span>
+		<select class="searchfrom">
+            <option value="any" selected>Anywhere in Line</option> 
+            <option value="begin">Beginning of Line</option>
+        </select>		
 		<input class="dark button" type="submit" value="Search"/>
 		<input class="reset-btn dark button" type="button" value="Reset"/>
 	</form>
