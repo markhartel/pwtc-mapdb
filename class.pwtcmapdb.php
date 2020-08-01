0ae8356ed4d9f73d51322b7b6c4f004ba4af72ea
markhartel <mark_hartel@hotmail.com>
2020-08-01 11:59:15 -0700

    Update class.pwtcmapdb.php

--- class.pwtcmapdb.php
+++ class.pwtcmapdb.php
@@ -600,12 +600,15 @@ class PwtcMapdb {
 			if ($now_date > $ride_date) {
 				return '<div class="callout small warning"><p>You cannot signup for ride "' . $ride_title . '" because it has already started. <em>The start time of the ride is ' . $ride_date_str . ' and the current time is ' . $now_date_str . '</em></p></div>';
 			}
-			/*
-			$now_time = $now_time - ($time_limit*60*60);
-			if ($now_time > $ride_time) {
-				return '<div class="callout small warning"><p>You cannot signup for ride "' . $ride_title . '" because it is within ' . $time_limit . ' hours of the start time. <em>The start time of the ride is ' . $ride_date_str . ' and the current time is ' . $now_date_str . '</em></p></div>';
+			//$now_time = $now_time - ($time_limit*60*60);
+			//if ($now_time > $ride_time) {
+			if ($time_limit > 0) {
+				$interval = new DateInterval('PT' . $time_limit . 'H');	
+				$ride_date.sub($interval);
+				if ($now_date > $ride_date) {
+					return '<div class="callout small warning"><p>You cannot signup for ride "' . $ride_title . '" because it is within ' . $time_limit . ' hours of the start time. <em>The start time of the ride is ' . $ride_date_str . ' and the current time is ' . $now_date_str . '</em></p></div>';
+				}
 			}
-			*/
 		}
 
 		if (isset($_POST['accept_user_signup'])) {
