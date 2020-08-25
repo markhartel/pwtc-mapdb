1767e4203dc66e99aa05bdc3dcc69786686754b9
markhartel <mark_hartel@hotmail.com>
2020-08-25 12:37:02 -0700

    Update class.pwtcmapdb.php

--- class.pwtcmapdb.php
+++ class.pwtcmapdb.php
@@ -1242,8 +1242,10 @@ class PwtcMapdb {
 			<form method="POST">
 		<?php if ($paperless) { ?>
 			<?php if ($signup_locked) { ?>
-				<a class="log_mileage dark button float-left"><i class="fa fa-bicycle"></i> Log Mileage</a>
+				<div class="button-group float-left">
+				<a class="log_mileage dark button"><i class="fa fa-bicycle"></i> Log Mileage</a>
 				<button class="dark button" type="submit" name="unlock_signup"><i class="fa fa-unlock"></i> Unlock Signup</button>
+				</div>
 			<?php } else { ?>
 				<button class="dark button float-left" type="submit" name="lock_signup" <?php echo $now_date < $cutoff_date ? 'disabled': ''; ?>><i class="fa fa-lock"></i> Lock Signup</button>
 			<?php } ?>
