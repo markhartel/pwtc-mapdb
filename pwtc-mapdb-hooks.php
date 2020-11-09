10c225aea2d60c8cd2560c8fc830f8ef1dd0d5bf
markhartel <mark_hartel@hotmail.com>
2020-11-09 10:56:44 -0800

    Update pwtc-mapdb-hooks.php

--- pwtc-mapdb-hooks.php
+++ pwtc-mapdb-hooks.php
@@ -1,6 +1,8 @@
 <?php
 
 function pwtc_mapdb_get_signup() {
+    $current_user = wp_get_current_user();
+    $now = PwtcMapdb::get_current_time();
     $postid = get_the_ID();
     $signup_mode = get_post_meta($postid, PwtcMapdb::RIDE_SIGNUP_MODE, true);
     if (!$signup_mode) {
@@ -12,17 +14,27 @@ function pwtc_mapdb_get_signup() {
     }
     $members_only = get_post_meta($postid, PwtcMapdb::RIDE_SIGNUP_MEMBERS_ONLY, true);
     $signup_locked = get_post_meta($postid, PwtcMapdb::RIDE_SIGNUP_LOCKED, true);
+    $start = PwtcMapdb::get_ride_start_time($postid);
 
     $result['view_signup_url'] = '/ride-view-signups/?post='.$postid;
-    $result['edit_ride_url'] = '/ride-edit-fields/?post='.$postid;
-    $result['copy_ride_url'] = '/ride-edit-fields/?post='.$postid.'&action=copy';
-
+    if ($start > $now) {
+        $result['edit_ride_url'] = '/ride-edit-fields/?post='.$postid;
+    }
+    else {
+        $result['edit_ride_url'] = false;
+    }
+    if (user_can($current_user,'edit_published_rides')) {
+        $result['copy_ride_url'] = '/ride-edit-fields/?post='.$postid.'&action=copy';
+    }
+    else {
+        $result['copy_ride_url'] = false;
+    }
     if ($signup_mode == 'no' or $signup_locked) {
         $result['ride_signup_msg'] = false;
         $result['ride_signup_url'] = false;
     }
     else {
-        if (wp_get_current_user()->ID != 0) {
+        if ($current_user->ID != 0) {
             $result['ride_signup_url'] = '/ride-online-signup/?post='.$postid;
             if ($signup_mode == 'paperless') {
                 $result['ride_signup_msg'] = 'You <em>must</em> sign up online to attend this ride.';
