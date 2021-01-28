8761fe2343cb842e9b9303747e175428941da22a
markhartel <mark_hartel@hotmail.com>
2021-01-28 15:00:43 -0800

    Update pwtc-mapdb.php

--- pwtc-mapdb.php
+++ pwtc-mapdb.php
@@ -24,7 +24,9 @@ register_uninstall_hook( __FILE__, array( 'PwtcMapdb', 'plugin_uninstall' ) );
 
 require_once( PWTC_MAPDB__PLUGIN_DIR . 'pwtc-mapdb-hooks.php' );
 require_once( PWTC_MAPDB__PLUGIN_DIR . 'class.pwtcmapdb.php' );
+require_once( PWTC_MAPDB__PLUGIN_DIR . 'class.pwtcmapdb-ride.php' );
 require_once( PWTC_MAPDB__PLUGIN_DIR . 'class.pwtcmapdb-admin.php' );
 
 add_action( 'init', array( 'PwtcMapdb', 'init' ) );
+add_action( 'init', array( 'PwtcMapdb_Ride', 'init' ) );
 add_action( 'init', array( 'PwtcMapdb_Admin', 'init' ) );
