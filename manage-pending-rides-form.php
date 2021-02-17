b94dc7edca2bc13f9a5320e7d71ae686338608a1
markhartel <mark_hartel@hotmail.com>
2021-02-16 16:44:37 -0800

    Update manage-pending-rides-form.php

--- manage-pending-rides-form.php
+++ manage-pending-rides-form.php
@@ -3,7 +3,6 @@
 	});
 </script>			
 <div id="pwtc-mapdb-manage-pending-rides-div">
-    <p>Hello road captain, the following rides are pending and ready for your review:</p>
     <?php if ($query->have_posts()) { ?>
     <table class="pwtc-mapdb-rwd-table">
         <thead><tr><th>Start Time</th><th>Ride Title</th><th>Author</th><th>Actions</th></tr></thead>
