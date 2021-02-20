a422fb33a7775d4aae02aeeecea9c1eab9feb96a
markhartel <mark_hartel@hotmail.com>
2021-02-19 16:34:46 -0800

    Update ride-pending-form.php

--- ride-pending-form.php
+++ ride-pending-form.php
@@ -17,7 +17,9 @@
     });
 </script>
 <div id='pwtc-mapdb-edit-ride-div'>
-    <div class="callout small warning"><p>Ride "<?php echo $ride_title; ?>" is pending review so you cannot edit it. <?php echo $return_to_ride; ?> If you want to revert this ride back to draft, click <a class="revert-action">here.</a></p></div>
+    <div class="callout small success">
+        <p>The draft ride "<?php echo $ride_title; ?>" was submitted for review so you can no longer edit it, click <a href="<?php echo $notify_link; ?>">here</a> to notify a road captain via email that it is ready to review. <?php echo $return_to_ride; ?> Click <a class="revert-action">here</a> if you want to revert this ride back to draft.</p>
+    </div>
     <form method="POST">
         <input type="hidden" name="postid" value="<?php echo $postid; ?>"/>
         <input type="hidden" name="revert" value="draft"/>
