5a9d65679f5b545f59491ebea2756951613accba
markhartel <mark_hartel@hotmail.com>
2021-02-28 13:05:29 -0800

    Update pwtc-mapdb-hooks.php

--- pwtc-mapdb-hooks.php
+++ pwtc-mapdb-hooks.php
@@ -3,7 +3,7 @@
 function pwtc_mapdb_get_signup() {
     $disable = false;
     $disable_nonmembers = true;
-    $disable_edit = true;
+    $disable_edit = false;
     if ($disable) {
         $result['view_signup_url'] = false;
         $result['edit_ride_url'] = false;
@@ -13,12 +13,10 @@ function pwtc_mapdb_get_signup() {
         return $result;
     }
     $current_user = wp_get_current_user();
-    $now = PwtcMapdb::get_current_time();
     $postid = get_the_ID();
     $signup_mode = PwtcMapdb_Signup::get_signup_mode($postid);
     $members_only = PwtcMapdb_Signup::get_signup_members_only($postid);
     $signup_locked = PwtcMapdb_Signup::get_signup_locked($postid);
-    $start = PwtcMapdb::get_ride_start_time($postid);
 
     $result['view_signup_url'] = '/ride-view-signups/?post='.$postid;
     
@@ -27,18 +25,12 @@ function pwtc_mapdb_get_signup() {
         $result['copy_ride_url'] = false;
     }
     else {
-        $return_uri = urlencode($_SERVER['REQUEST_URI']);
+        $return_uri = $_SERVER['REQUEST_URI'];
         $result['edit_ride_url'] = false;
-        if (user_can($current_user,'edit_published_rides') and $start > $now) {
-            $result['edit_ride_url'] = esc_url('/ride-edit-fields/?post='.$postid.'&return='.$return_uri);
+        if (user_can($current_user,'edit_published_rides')) {
+            $result['edit_ride_url'] = PwtcMapdb_Ride::edit_ride_link($postid, $return_uri);
         }
         $result['copy_ride_url'] = false;
-        if ($current_user->ID != 0) {
-            $user_info = get_userdata($current_user->ID);
-            if (user_can($current_user,'edit_published_rides') or in_array(PwtcMapdb::ROLE_RIDE_LEADER, $user_info->roles)) {
-                $result['copy_ride_url'] = esc_url('/ride-edit-fields/?post='.$postid.'&action=copy&return='.$return_uri);
-            }
-        }
     }
 
     if ($signup_mode == 'no' or $signup_locked) {
