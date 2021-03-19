752108bfefbc641d8f089f8c6ed0f65d8c0f28b7
markhartel <mark_hartel@hotmail.com>
2021-03-19 15:03:04 -0700

    Update ride-pending-form.php

--- ride-pending-form.php
+++ ride-pending-form.php
@@ -10,13 +10,13 @@
     });
 </script>
 <div id='pwtc-mapdb-edit-ride-div'>
+    <?php echo $return_to_ride; ?>
     <div class="callout small success">
         <p>The draft ride "<?php echo $ride_title; ?>" was submitted for review
         <?php if ($email_status == 'yes') { ?> and the road captain notified by email
         <?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>.
         <a class="revert-action">Undo</a></p>
     </div>
-    <p><?php echo $return_to_ride; ?></p>
     <form method="POST">
         <input type="hidden" name="postid" value="<?php echo $postid; ?>"/>
         <input type="hidden" name="revert" value="draft"/>
