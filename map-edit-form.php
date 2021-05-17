173928b442840f83a824bf54e88cdcc6eab7d675
markhartel <mark_hartel@hotmail.com>
2021-05-16 17:18:57 -0700

    Add files via upload

---
+++ map-edit-form.php
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+    jQuery(document).ready(function($) { 
+    });
+</script>
+<div id='pwtc-mapdb-edit-map-div'>
+    <form method="post" enctype="multipart/form-data">
+        <label for="exampleFileUpload" class="dark button">Upload File</label>
+        <input type="file" id="exampleFileUpload" class="show-for-sr" multiple="false">
+    </form>
+</div>
+<?php 
