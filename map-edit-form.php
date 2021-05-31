acd392ac9a2bdf6edc85d6b6755e6309bf44b3bf
markhartel <mark_hartel@hotmail.com>
2021-05-31 16:11:24 -0700

    Update map-edit-form.php

--- map-edit-form.php
+++ map-edit-form.php
@@ -1,27 +1,110 @@
+<style>
+    .indicate-error {
+        border-color: #900 !important;
+        background-color: #FDD !important;
+    }
+</style>
 <script type="text/javascript">
     jQuery(document).ready(function($) { 
-
-        $('#exampleFileUpload').on('change', function(e) {
-            var fileName = '';
-            if (e.target.value) {
-				fileName = e.target.value.split('\\').pop();
-            }
-			if (fileName) {
-				$('#pwtc-mapdb-edit-map-div form div').html(fileName);
-            }
-			else {
-				$('#pwtc-mapdb-edit-map-div form div').html('');
-            }
-        });
-
     });
 </script>
 <div id='pwtc-mapdb-edit-map-div'>
-    <form method="post" enctype="multipart/form-data">
-        <label for="exampleFileUpload" class="dark button">Select File for Upload</label>
-        <input type="file" id="exampleFileUpload" class="show-for-sr" multiple="false" name="map_file">
-        <div></div>
-        <input class="dark button" type="submit" name="upload_file" value="Upload File">
-    </form>
+    <?php echo $return_to_map; ?>
+    <?php if (!empty($operation)) { ?>
+    <div class="callout small success">
+        <p>
+        <?php if ($operation == 'update_draft') { ?>
+        The draft route map was updated.
+        <?php } else if ($operation == 'submit_review') { ?>
+        The draft route map was submitted for review.
+        <?php } else if ($operation == 'update_pending') { ?>
+        The pending route map was updated.
+        <?php } else if ($operation == 'published_draft') { ?>
+        The draft route map was published.
+        <?php } else if ($operation == 'published') { ?>
+        The pending route map was published
+        <?php if ($email_status == 'yes') { ?> and the author notified by email
+        <?php } else if ($email_status == 'failed') { ?> but failed to notify author by email<?php } ?>.
+        <?php } else if ($operation == 'rejected') { ?>
+        The pending route map was rejected
+        <?php if ($email_status == 'yes') { ?> and the author notified by email
+        <?php } else if ($email_status == 'failed') { ?> but failed to notify author by email<?php } ?>.
+        <?php } else if ($operation == 'update_published') { ?>
+        The published route map was updated.
+        <?php } else if ($operation == 'unpublished') { ?>
+        The published route map was unpublished.
+        <?php } else if ($operation == 'insert') { ?>
+        The first draft of your route map was saved.
+        <?php } else if ($operation == 'revert_draft') { ?>
+        The route map was reverted back to draft
+        <?php if ($email_status == 'yes') { ?> and the road captain notified by email
+        <?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>.
+        <?php } ?>
+        </p>
+    </div>
+    <?php } ?>
+    <div>
+        <p>
+    <?php if ($postid != 0) { ?>
+        This route map was authored by 
+        <?php if ($author != $current_user->ID) { 
+            echo '<a href="' . esc_url('mailto:' . $author_email) . '">' . $author_name . '</a>';
+        } else { 
+            echo $author_name;
+        } ?> and is 
+        <?php if ($status == 'draft') { ?>
+        a draft. It can be updated or <?php if ($allow_leaders and !$is_road_captain) { ?>submitted for review<?php } else { ?>published<?php } ?> using the buttons at the bottom of the form.
+        <?php } else if ($status == 'pending') { ?>
+        pending review by a road captain. It can be updated, published or rejected using the buttons at the bottom of the form.
+        <?php } else if ($status == 'publish') { ?>
+        published and ready for use. It can be updated or unpublished using the buttons at the bottom of the form.
+        <?php } ?>
+    <?php } else { ?>
+        <?php if ($copy_ride) { ?>
+        This is a new route map copied from an existing route map, set the <em>ride date</em> in the form below (and modify any other desired fields) and press the save button at the bottom of the form.
+        <?php } else { ?>
+        This is a new route map, fill out the form below and press the save button at the bottom of the form.
+        <?php } ?>
+    <?php } ?>
+        </p>
+    </div>
+    <div class="callout">
+        <form method="POST" novalidate>
+            <?php wp_nonce_field('map-edit-form', 'nonce_field'); ?>
+            <div class="row column">
+                <label>Route Map Title
+                    <input type="text" name="title" value="<?php echo esc_attr($title); ?>"/>
+                    <input type="hidden" name="postid" value="<?php echo $postid; ?>"/>
+                    <input type="hidden" name="post_status" value="<?php echo $status; ?>"/>
+                </label>
+            </div>
+            <div class="row column errmsg"></div>
+            <div class="row column clearfix">
+            <?php if ($postid == 0) { ?>
+                <button class="dark button float-left" type="submit">Save Draft</button>
+            <?php } else if ($status == 'draft') { ?>
+                <div class="button-group float-left">
+                    <input class="dark button" name="draft" value="Update" type="submit"/>
+                    <?php if ($allow_leaders and !$is_road_captain) { ?>
+                    <input class="dark button" name="pending" value="Submit for Review" type="submit"/>
+                    <?php } else { ?>
+                    <input class="dark button" name="publish" value="Publish" type="submit"/>
+                    <?php } ?>
+                </div>
+            <?php } else if ($status == 'pending') { ?>
+                <div class="button-group float-left">
+                    <input class="dark button" name="pending" value="Update" type="submit"/>
+                    <input class="dark button" name="publish" value="Publish" type="submit"/>
+                    <input class="dark button" name="draft" value="Reject" type="submit"/>
+                </div>
+            <?php } else { ?>
+                <div class="button-group float-left">
+                    <input class="dark button" name="publish" value="Update" type="submit"/>
+                    <input class="dark button" name="draft" value="Unpublish" type="submit"/>
+                </div>
+            <?php } ?>
+            </div>
+        </form>
+    </div>
 </div>
 <?php 
