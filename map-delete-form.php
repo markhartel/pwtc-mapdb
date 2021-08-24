39d201fefb3cf559dc7da259b6da6b411eb02fe9
markhartel <mark_hartel@hotmail.com>
2021-08-24 16:18:20 -0700

    Update map-delete-form.php

--- map-delete-form.php
+++ map-delete-form.php
@@ -33,7 +33,7 @@
             if ( data['pwtc-refresh-post-lock'] ) {
                 var received = data['pwtc-refresh-post-lock'];
                 if ( received.lock_error ) {
-                    $('#pwtc-mapdb-delete-map-div').html('<div class="callout small alert">You cannot delete route map "<?php echo $map_title; ?>". ' + received.lock_error.text + '</div>');
+                    $('#pwtc-mapdb-delete-map-div').html('<div class="callout small alert">You cannot delete post "<?php echo $map_title; ?>". ' + received.lock_error.text + '</div>');
                 } 
                 else if ( received.new_lock ) {
                 }
