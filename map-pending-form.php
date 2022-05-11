3248a96c28ad6acaca90e5cf7d4f8ffc47695e51
markhartel <mark_hartel@hotmail.com>
2022-05-11 16:31:05 -0700

    Update map-pending-form.php

--- map-pending-form.php
+++ map-pending-form.php
@@ -12,19 +12,14 @@
 <div id='pwtc-mapdb-edit-map-div'>
     <?php echo $return_to_map; ?>
     <div class="callout small success">
-        <p>The draft route map "<?php echo $map_title; ?>" was submitted for review
-        <?php if ($email_status == 'yes') { ?> and the road captain notified by email
-        <?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>. 
-        <a class="revert-action">Undo</a></p>
+        <p>The draft route map "<?php echo $map_title; ?>" was submitted for review<?php if ($email_status == 'yes') { ?> and the road captain notified by email<?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>.</p>
     </div>
     <div class="row column">
-        <p class="help-text">Do you wish to create additional route maps or rides? If yes, you may use the convenience buttons below.</p>
-    </div>
-    <div class="row column clearfix">
-        <div class="button-group float-left">
-            <a class="dark button" href="<?php echo $create_map_link; ?>">Create Another Map</a>
-            <a class="dark button" href="<?php echo $create_ride_link; ?>">Create New Ride</a>
-        </div>
+        <p>Did you submit this map by mistake? If so, <a class="revert-action">undo the submission.</a></p>
+        <p>Do you wish to submit additional route maps or rides? If so, you may use the convenience links below.<ul>
+            <li><a href="<?php echo $create_map_link; ?>">Submit another new route map.</a></li>
+            <li><a href="<?php echo $create_ride_link; ?>">Submit a new ride.</a></li>
+        </ul></p>
     </div>
     <form method="POST">
         <?php wp_nonce_field('map-edit-form', 'nonce_field'); ?>
