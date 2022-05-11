5812bab9565a15dcd874cd84a24cf20db6fbfadd
markhartel <mark_hartel@hotmail.com>
2022-05-11 16:28:25 -0700

    Update ride-pending-form.php

--- ride-pending-form.php
+++ ride-pending-form.php
@@ -12,20 +12,15 @@
 <div id='pwtc-mapdb-edit-ride-div'>
     <?php echo $return_to_ride; ?>
     <div class="callout small success">
-        <p>The draft ride "<?php echo $ride_title; ?>" was submitted for review
-        <?php if ($email_status == 'yes') { ?> and the road captain notified by email
-        <?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>.
-        <a class="revert-action">Undo</a></p>
+        <p>The draft ride "<?php echo $ride_title; ?>" was submitted for review<?php if ($email_status == 'yes') { ?> and the road captain notified by email<?php } else if ($email_status == 'failed') { ?> but failed to notify road captain by email<?php } ?>.</p>
     </div>
     <div class="row column">
-        <p class="help-text">Do you wish to create additional rides or route maps? If yes, you may use the convenience buttons below.</p>
-    </div>
-    <div class="row column clearfix">
-        <div class="button-group float-left">
-            <a class="dark button" href="<?php echo $copy_ride_link; ?>">Copy This Ride</a>
-            <a class="dark button" href="<?php echo $create_ride_link; ?>">Create Another Ride</a>
-            <a class="dark button" href="<?php echo $create_map_link; ?>">Create New Map</a>
-        </div>
+        <p>Did you submit this ride by mistake? If so, <a class="revert-action">undo the submission.</a></p>
+        <p>Do you wish to submit additional rides or route maps? If so, you may use the convenience links below.<ul>
+            <li><a href="<?php echo $copy_ride_link; ?>">Submit a copy of this ride.</a></li>
+            <li><a href="<?php echo $create_ride_link; ?>">Submit another new ride.</a></li>
+            <li><a href="<?php echo $create_map_link; ?>">Submit a new route map.</a></li>
+        </ul></p>
     </div>
     <form method="POST">
         <?php wp_nonce_field('ride-edit-form', 'nonce_field'); ?>
