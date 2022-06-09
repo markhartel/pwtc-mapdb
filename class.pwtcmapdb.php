26a714780ccbfe2eb68975f469efb0245407a609
markhartel <mark_hartel@hotmail.com>
2022-06-09 11:12:31 -0700

    Update class.pwtcmapdb.php

--- class.pwtcmapdb.php
+++ class.pwtcmapdb.php
@@ -411,13 +411,13 @@ class PwtcMapdb {
 		//error_log(print_r($maps, true));
 		if (count($maps) > 0) {
 			if ($maps[0][self::MAP_TYPE_FIELD] == 'link') {
-				$link = '<a title="Display online ride route map." href="' . $maps[0][self::MAP_LINK_FIELD] . '" target="_blank"><i class="fa fa-link"></i></a>';
+				$link = '<a title="Display online ride route map." href="' . $maps[0][self::MAP_LINK_FIELD] . '" target="_blank"><i class="fa fa-link"></i> GPS</a>';
 			}
 			else if ($maps[0][self::MAP_TYPE_FIELD] == 'file') {
-				$link = '<a title="Download ride route map file." href="' . $maps[0][self::MAP_FILE_FIELD]['url'] . '" target="_blank" download><i class="fa fa-download"></i></a>';
+				$link = '<a title="Download ride route map file." href="' . $maps[0][self::MAP_FILE_FIELD]['url'] . '" target="_blank" download><i class="fa fa-download"></i> PDF</a>';
 			}
 			else if ($maps[0][self::MAP_TYPE_FIELD] == 'both') {
-				$link = '<a title="Download ride route map file." href="' . $maps[0][self::MAP_FILE_FIELD]['url'] . '" target="_blank" download><i class="fa fa-download"></i></a> <a title="Display online ride route map." href="' . $maps[0][self::MAP_LINK_FIELD] . '" target="_blank"><i class="fa fa-link"></i></a>';
+				$link = '<a title="Download ride route map file." href="' . $maps[0][self::MAP_FILE_FIELD]['url'] . '" target="_blank" download><i class="fa fa-download"></i> PDF</a> <a title="Display online ride route map." href="' . $maps[0][self::MAP_LINK_FIELD] . '" target="_blank"><i class="fa fa-link"></i> GPS</a>';
 			}
 		}
 		return $link;
