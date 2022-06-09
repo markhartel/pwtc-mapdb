0326ddd074d40317f0317c0710634781734c2685
markhartel <mark_hartel@hotmail.com>
2022-06-09 16:55:58 -0700

    Add files via upload

---
+++ map-usage-form.php
@@ -0,0 +1,7 @@
+<div id="pwtc-mapdb-usage-map-div">
+    <?php echo $return_to_map; ?>
+    <div class="row column">
+        <p>The route map "<?php echo $map_title; ?>" is used by the following ride templates and scheduled rides.</p>
+    </div>
+</div>
+<?php 
