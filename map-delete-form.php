26a66136f642ab52842dc819f182bb9da1fee590
markhartel <mark_hartel@hotmail.com>
2022-06-10 14:40:09 -0700

    Update map-delete-form.php

--- map-delete-form.php
+++ map-delete-form.php
@@ -68,7 +68,7 @@
     <?php } ?>
     <?php if ($used_in_ride) { ?>
         <div class="callout small warning">
-            <p>Warning, this route map is used by a scheduled ride. It can be deleted, but will then be unavailable to the ride.</p>
+            <p>Warning, this route map is used by a ride template or scheduled ride. It can be deleted, but will then be unavailable to the ride.</p>
         </div>
     <?php } ?>
         <div class="callout">
