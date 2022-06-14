6fb8e7716f22c4806dadf64baa782a4e037c1fb7
markhartel <mark_hartel@hotmail.com>
2022-06-13 21:06:30 -0700

    Update manage-published-rides-form.php

--- manage-published-rides-form.php
+++ manage-published-rides-form.php
@@ -47,7 +47,7 @@
     <div class="row column">
         <form class="sort-frm" method="POST" novalidate>
             <input type="hidden" name="ride_status" value="<?php echo $ride_status; ?>">
-            <input type="hidden" name="ride_title" value="<?php echo $ride_title; ?>">
+            <input type="hidden" name="ride_title" value="<?php echo stripslashes($ride_title); ?>">
             <input type="hidden" name="ride_leader" value="<?php echo $ride_leader; ?>">
             <input type="hidden" name="ride_month" value="<?php echo $ride_month; ?>">
             <input type="hidden" name="offset" value="0">
@@ -85,7 +85,7 @@
                     <?php } ?>
                         <div class="small-12 medium-4 columns">
                             <label>Ride Title 
-                                <input type="text" name="ride_title" value="<?php echo $ride_title; ?>">
+                                <input type="text" name="ride_title" value="<?php echo stripslashes($ride_title); ?>">
                             </label>
                         </div>
                         <div class="small-12 medium-3 columns">
@@ -190,7 +190,7 @@
     <?php if ($is_more or $is_prev) { ?>
     <form class="load-more-frm" method="POST">
         <input type="hidden" name="ride_status" value="<?php echo $ride_status; ?>">
-        <input type="hidden" name="ride_title" value="<?php echo $ride_title; ?>">
+        <input type="hidden" name="ride_title" value="<?php echo stripslashes($ride_title); ?>">
         <input type="hidden" name="ride_leader" value="<?php echo $ride_leader; ?>">
         <input type="hidden" name="ride_month" value="<?php echo $ride_month; ?>">
         <input type="hidden" name="sort_by" value="<?php echo $sort_by; ?>">
