a6a9ac8d81bc89a6a9597ea0f9bf85e64de71c12
markhartel <mark_hartel@hotmail.com>
2022-06-17 14:36:40 -0700

    Update map-usage-form.php

--- map-usage-form.php
+++ map-usage-form.php
@@ -2,21 +2,30 @@
     <?php echo $return_to_map; ?>
     <div class="row column">
 <?php
-    if ($template_query->have_posts() or $ride_query->have_posts()) {
+    $template_count = 0;
+    $ride_count = 0;
+    if ($template_query->have_posts()) {
 ?>
-        <p>The route map "<?php echo $map_title; ?>" is used by the following ride templates and scheduled rides:
+        <p>The route map "<?php echo $map_title; ?>" is used by the following ride templates:
 <?php
-        $count = 0;
         while ($template_query->have_posts()) {
             $template_query->the_post();
             $title = esc_html(get_the_title());
             $view_link = esc_url(get_the_permalink());
 ?>
-            <?php if ($count > 0) { ?>, <?php } ?><a href="<?php echo $view_link; ?>"><?php echo $title; ?></a>
+            <?php if ($template_count > 0) { ?>, <?php } ?><a href="<?php echo $view_link; ?>"><?php echo $title; ?></a>
 <?php
-            $count++;
+            $template_count++;
         }
         wp_reset_postdata();
+?>
+        </p>
+<?php
+    }
+    if ($ride_query->have_posts()) {
+?>
+        <p>The route map "<?php echo $map_title; ?>" is used by the following scheduled rides:
+<?php
         while ($ride_query->have_posts()) {
             $ride_query->the_post();
             $postid = get_the_ID();
@@ -25,20 +34,21 @@
             $start_date = $start->format('m/d/Y');
             $view_link = esc_url(get_the_permalink());
 ?>
-            <?php if ($count > 0) { ?>, <?php } ?><a href="<?php echo $view_link; ?>"><?php echo $title; ?> (<?php echo $start_date; ?>)</a>
+            <?php if ($ride_count > 0) { ?>, <?php } ?><a href="<?php echo $view_link; ?>"><?php echo $title; ?> (<?php echo $start_date; ?>)</a>
 <?php
-            $count++;
+            $ride_count++;
         }
         wp_reset_postdata();
 ?>
         </p>
 <?php
-    } else {
-?> 
-        <p>The route map "<?php echo $map_title; ?>" is not used by any ride templates or scheduled rides.</p>
-<?php       
     }
+    if ($template_count == 0 and $ride_count == 0) {
 ?>
+        <p>The route map "<?php echo $map_title; ?>" is not used by any ride templates or scheduled rides.</p> 
+<?php
+    } 
+?>       
     </div>
 </div>
 <?php 
