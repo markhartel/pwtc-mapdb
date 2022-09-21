b95c55c594b7d04c064b29ef09d5fdade7ab28e2
markhartel <mark_hartel@hotmail.com>
2022-09-21 16:07:28 -0700

    Update ride-edit-form.php

--- ride-edit-form.php
+++ ride-edit-form.php
@@ -1412,9 +1412,10 @@
             <?php } else if ($status == 'draft') { ?>
                 <div class="button-group float-left">
                     <input class="dark button" name="draft" value="Update" type="submit"/>
-                    <?php if ($allow_leaders and !$is_road_captain) { ?>
+		    <?php if ($allow_leaders) { ?>
                     <input class="dark button" name="pending" value="Submit for Review" type="submit"/>
-                    <?php } else { ?>
+                    <?php } ?>
+                    <?php if ($is_road_captain) { ?>
                     <input class="dark button" name="publish" value="Publish" type="submit"/>
                     <?php } ?>
                 </div>
