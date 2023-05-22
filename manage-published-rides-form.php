2000924f5618fc40b2af49c5c0d18d1871f980ca
markhartel <mark_hartel@hotmail.com>
2023-05-22 16:36:26 -0700

    Update manage-published-rides-form.php

--- manage-published-rides-form.php
+++ manage-published-rides-form.php
@@ -44,7 +44,7 @@
 </script>			
 <div id="pwtc-mapdb-manage-published-rides-div">
 <?php if ($monthonly) { ?>
-    <a class="dark button" href="/scheduled_rides/?month="<?php echo $ride_month; ?>"><i class="fa fa-calendar" aria-hidden="true"></i> Calendar View</a>
+    <a class="dark button" href="/scheduled_rides/?month=<?php echo $ride_month; ?>"><i class="fa fa-calendar" aria-hidden="true"></i> Calendar View</a>
 <?php } ?>
 <?php if ($is_road_captain) { ?>
     <div class="row column">
