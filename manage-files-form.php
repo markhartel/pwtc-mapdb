6257ab18fbbbde2e150989a624299e3beb02d0fc
markhartel <mark_hartel@hotmail.com>
2023-06-05 16:22:53 -0700

    Update manage-files-form.php

--- manage-files-form.php
+++ manage-files-form.php
@@ -85,8 +85,6 @@
     <?php if ($query->have_posts()) { 
     $total = $query->found_posts;
     $warn = $total > $limit;
-    $is_more = ($limit > 0) && ($total > ($offset + $limit));
-    $is_prev = ($limit > 0) && ($offset > 0);
     ?>
     <?php if ($warn) { ?>
     <div class="callout small warning">
@@ -121,24 +119,12 @@
     ?>
         </tbody>
     </table>
-    <?php if ($is_more or $is_prev) { ?>
+    <?php if ($warn) { ?>
     <form class="load-more-frm" method="POST">
         <input type="hidden" name="file_title" value="<?php echo stripslashes($file_title); ?>">
         <input type="hidden" name="sort_by" value="<?php echo $sort_by; ?>">
         <div class="row column errmsg"></div>
-        <div class="row column clearfix">
-            <div class="button-group float-left">
-            <?php if ($is_prev) { ?>
-                <button class="dark button" type="submit" name="offset" value="<?php echo $offset - $limit; ?>">Show Previous <?php echo $limit; ?> Files</button>
-            <?php } ?>
-            <?php if ($is_more) { ?>
-                <button class="dark button" type="submit" name="offset" value="<?php echo $offset + $limit; ?>">Show Next <?php echo $limit; ?> Files</button>
-            <?php } ?>
-            </div>
-            <?php if ($is_more) { ?>
-            <label class="float-right">Remaining files: <?php echo ($total - ($offset + $limit)); ?></label>
-            <?php } ?>
-        </div>
+        <?php echo PwtcMapdb::output_pagination_html($limit, $offset, $total); ?>
     </form>
     <?php } ?>
     <?php } else { ?>
